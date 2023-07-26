753977e5902d29e03d6081605b8c84c68317bad7
gabriel2020 <gabrielbustamante007@gmail.com>
2023-07-25 21:52:24 -0500

    [FIX] Correcion de ortografía

--- src/components/About.js
+++ src/components/About.js
@@ -35,15 +35,16 @@ export const About = () => {
                             <h2>
                                 Sobre Mí
                             </h2>
-                            <p className="parrafo">Desarrollador Backend y Frontend. En conocimientos de lenguaje de programación PHP, Javascript. Framework Laravel, React js, Graphql, diseño de Base de Datos relacionales (SQL, MySql) para el desarrollo de Aplicaciones y Páginas web.
+                            <p className="parrafo">Desarrollador Backend y Frontend. En conocimientos de lenguaje de programación PHP, Javascript, Ruby. Framework Laravel, ReactJS, NextJS, Ruby on Rails, Graphql, diseño de Base de Datos relacionales (SQL, MySql) para el desarrollo de Aplicaciones y Páginas web.
                                 <br/>
-                                Soy una persona siempre dispuesta aprender nuevas cosas, me gustan los retos. La empatia y perserverancia son unas de mis cualidades mas resaltante, siempre me pongo en el lugar de las personas y trato de entender el por qué de las cosas, comprender situaciones de la vida o decisiones sin juzgar a nadie.<br/> Perserverante por que soy un hombre lleno de metas a corto y largo plazo trabajo en pro hasta terminar de logralas.
+                                Soy una persona siempre dispuesta aprender nuevas cosas, me gustan los retos. La empatía y perseverancia son unas de mis cualidades más resaltante, siempre me pongo en el lugar de las personas y trato de entender el porqué de las cosas, comprender situaciones de la vida o decisiones sin juzgar a nadie. <br/> Perseverante porque soy un hombre lleno de metas a corto y largo plazo trabajo en pro hasta terminar de lógralas.
                                 <br/>
-                                Hay una frase que me gusta mucho y que aplica de manera general y es lo que dijo Martin Luther King Jr.<em> "Tus talentos y habilidades irán mejorando con el tiempo, pero para eso has de empezar"</em>, esta se refiere a la importancia de comenzar a trabajar en nuestras metas y sueños, incluso si no nos sentimos completamente preparados o seguros de nuestras habilidades. El mensaje detrás de esta cita es que el crecimiento personal y el desarrollo de habilidades son un proceso continuo que requiere tiempo y esfuerzo.<br/>
+                                Hay una frase que me gusta mucho y que aplica de manera general y es lo que dijo Martin Luther King Jr.<em> "Tus talentos y habilidades irán mejorando con el tiempo, pero para eso has de empezar"</em>, esta se refiere a la importancia de comenzar a trabajar en nuestras metas y sueños, incluso si no nos sentimos completamente preparados o seguros de nuestras habilidades. El mensaje detrás de esta cita es que el crecimiento personal y el desarrollo de habilidades son un proceso continuo que requiere tiempo y esfuerzo. <br/>
                                 Esta frase nos recuerda que no debemos permitir que el miedo o la inseguridad nos detengan en la búsqueda de nuestros sueños y metas. En lugar de eso, debemos tener confianza en nuestras habilidades y dar el primer paso hacia nuestros objetivos, sabiendo que podemos mejorar y crecer a lo largo del camino.
                                 <br/>
                                 <br/>
                                 <em>"Siempre comprometido con mis metas y sueños y por supuesto con la verdadera sinceridad."</em> - Gabriel Bustamante</p>
+
                         </div>
                     </Col>
                 </Row>
